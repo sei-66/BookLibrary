18d0b923aa44c2fef291da95217a9f2ecd3d2c07
sei-66 <seina623@icloud.com>
2022-11-14 23:27:18 +0900

    2

--- booklib/settings.py
+++ booklib/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
-import dotenv import load_dotenv
+from dotenv import load_dotenv
 load_dotenv()
 
 
